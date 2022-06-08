6ed4ca150667a259e9aad7d04d0e18700dda532a
dpreston <dpreston@rational-liberty.com>
2022-06-09 03:05:55 +1000

    added ecto seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,32 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias UTCDateTime
+alias Mammoth.Repo
+alias Mammoth.Registrations.Registration
+
+registrations = [
+  %Registration{
+    email: "alice@example.com",
+    name: "Alice",
+    phone: "07 1234 5678",
+    session: ~D[2022-05-24]
+  },
+  %Registration{
+    email: "bob@example.com",
+    name: "Bob",
+    phone: "02 1234 5678",
+    session: ~D[2022-05-24]
+  },
+  %Registration{
+    email: "charles@example.com",
+    name: "Charles",
+    phone: "0412 345 678",
+    session: ~D[2022-05-25]
+  }
+]
+
+Enum.each(registrations, fn r ->
+  Repo.insert!(r)
+end)
