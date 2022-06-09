8a7cf318fc9780dc9d8bbcbf22643f9689026a6f
dpreston <dpreston@rational-liberty.com>
2022-06-09 16:24:05 +1000

    config changes for deployment

--- config/runtime.exs
+++ config/runtime.exs
@@ -22,7 +22,7 @@ end
 
 if config_env() == :prod do
   database_url =
-    System.get_env("DATABASE_URL") ||
+    System.get_env("MAMMOTH_DATABASE_URL") ||
       raise """
       environment variable DATABASE_URL is missing.
       For example: ecto://USER:PASS@HOST/DATABASE
@@ -42,14 +42,14 @@ if config_env() == :prod do
   # to check this value into version control, so we use an environment
   # variable instead.
   secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
+    System.get_env("MAMMOTH_SECRET_KEY_BASE") ||
       raise """
       environment variable SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  host = System.get_env("MAMMOTH_PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("MAMMOTH_PORT") || "4000")
 
   config :mammoth, MammothWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
