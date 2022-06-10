260a7a15afa38d3fefb73c77f0058e32e33d9675
dpreston <dpreston@rational-liberty.com>
2022-06-10 13:15:16 +1000

    added pubsub for live updating of the list page

--- lib/mammoth/registrations.ex
+++ lib/mammoth/registrations.ex
@@ -12,10 +12,25 @@ defmodule Mammoth.Registrations do
     %Registration{}
     |> Registration.changeset(attrs)
     |> Repo.insert()
+    |> broadcast(:new_registration)
   end
 
   def list_all() do
     Registration
     |> Repo.all()
+  end
+
+  def subscribe do
+    Phoenix.PubSub.subscribe(Mammoth.PubSub, "registrations")
+  end
+
+  defp broadcast({:error, _reason} = error, _event), do: error
+
+  defp broadcast({:ok, registration}, event) do
+    IO.inspect(registration, label: "broadcast: registration")
+    IO.inspect(event, label: "broadcast: event")
+    Phoenix.PubSub.broadcast(Mammoth.PubSub, "registrations", {event, registration})
+
+    {:ok, registration}
   end
 end
--- lib/mammoth_web/live/registration_live/list.ex
+++ lib/mammoth_web/live/registration_live/list.ex
@@ -5,6 +5,15 @@ defmodule MammothWeb.RegistrationLive.List do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, registrations: Registrations.list_all())}
+    if connected?(socket), do: Registrations.subscribe()
+    registrations = Registrations.list_all()
+
+    {:ok, assign(socket, registrations: registrations)}
+  end
+
+  @impl true
+  def handle_info({:new_registration, _registration}, socket) do
+    registrations = Registrations.list_all()
+    {:noreply, assign(socket, registrations: registrations)}
   end
 end
